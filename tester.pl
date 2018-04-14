29144c2acf7195820a55df9233e323e25881894c
Jason Wiker <jason.wiker@gmail.com>
2018-04-14 16:57:34 -0600

    Merge of Jesus' code

--- tester.pl
+++ tester.pl
@@ -22,12 +22,11 @@ machine(6).
 machine(7).
 machine(8).
 
-bad(1).
-bad(2).
-bad(b).
-bad(c).
-bad([a]).
-bad([b]).
+toonear(a,a).
+forcedpartial(a,0).
+forbidden(a,0).
+toonearp(a,a,0).
+
 findlowest:-
 	forall(testnode(X,Y),anus(X,Y)).
 	
@@ -41,12 +40,11 @@ anus(X,Y):-
 	  retractall(lowpen(Z)),
 	  asserta(lowpen(X))
 	).
+	
+:-dynamic lowlist/1.
+:-dynamic lowpen/1.
 lowlist([]).
 lowpen(9999999999999999).
-:-dynamic(
-lowlist/1,
-lowpen/1
-).
 
 nodevalid(0,[]).
 nodevalid(Something, [H|T]) :-
@@ -57,51 +55,88 @@ nodevalid(Something, [H|T]) :-
 	reverse(TR,TRR),
 	nodevalid(Something_less, TRR).
 	
-
 correctassign([]).
 correctassign([T|H]) :-
 	task(T),
 	correctassign(H).
 	
+%toonear(a,b).
+%toonear(g,f).
+%toonear(b,c).
+%toonear(d,e).
 
-toonear(a,b).
-toonear(g,f).
-forcedpartial(a,3).
+%forcedpartial(a,1).
+%forcedpartial(b,2).
+%forcedpartial(c,3).
+%forcedpartial(d,4).
+%forcedpartial(e,5).
 
-tncheck(Inputlist):-
-	forall(toonear(X,Y),closecheck(Inputlist,X,Y)).
+%forbidden(a,2).
+%forbidden(b,2).
+%forbidden(c,2).
+%forbidden(d,2).
+%forbidden(e,2).
 
-closecheck(Inputlist,X,Y):-
-	append([X],[Y],Z1),
-	append([Y],[X],Z2),
-	not(sublist(Z1,Inputlist)),
-	not(sublist(Z2,Inputlist)).
+hardcons(List):-
+	not(tntcheck(List)),
+	fpcheck(List),
+	fmcheck(List).
+
+tntcheck(List):-
+	forall(toonear(X,Y),closecheck(List,X,Y)).
+
+length_1(0,[]).
+length_1(L+1, [H|T]) :- length_1(L,T).
+
+closecheck(List,X,Y):-
+	X==Y;
+	proper_length(List,8),
+	Imputlist = [H|T],
+	reverse(List,Rlist),
+	Rlist = [HR|TR],
+	X==HR,
+	Y==H;
+	length_1(L,List),
+	M is L,
+	(M==1->true;
+	nth1(M,List,W),	
+	N is L-1,
+	nth1(N,List,V),
+	X==V,
+	Y==W).
 	
-fpcheck(Inlist,Indepth):-
-	forcedpartial(X,Y),
-	reverse(Inputlist,Reversed),
-	Reversed=[H|T],
-	Y=Inputdepth,
-	H=X.
+fpcheck(Inputlist):-
+	forall(forcedpartial(X,Y),forcedtaskcheck(Inputlist,X,Y)).
+	
+forcedtaskcheck(List,X,Y):-
+	not(proper_length(List,Y));
+	proper_length(List,Y),
+	last(List,X).	
+	
+fmcheck(Inputlist):-
+	forall(forbidden(X,Y),forbiddencheck(Inputlist,X,Y)).
+
+forbiddencheck(List,X,Y):-
+	not(proper_length(List,Y));
+	proper_length(List,Y),
+	not(last(List,X)).
 
 nodegrow(8,Y) :-
 	asserta(node(8,Y)).
 nodegrow(X,Y) :-
+	write("nodegrow"),
+	write(Y),
 	X2 is X+1,
 	subtract([a,b,c,d,e,f,g,h],Y,Remaining),
 	branchalot(X2,Y,Remaining).
 	
 branchalot(X,Y,[]).
 branchalot(X,Y,[H|T]) :-
-
+	write("branchalot"),
+	write(Y),
 	append(Y,[H],Total),
-	masterrule(X, Y) -> nodegrow(X,Total),
-
+	nodegrow(X,Total),
 	branchalot(X,Y,T).
-	
-sublist( [], _ ).
-sublist( [X|XS], [X|XSS] ) :- sublist( XS, XSS ).
-sublist( [X|XS], [_|XSS] ) :- sublist( [X|XS], XSS ).
 
 masterrule(Depth, Assignment ) :-
 	\+ (Assignment).
@@ -111,4 +146,18 @@ machinepen(Input, Returnpen, Depth) :-
     nth1(Depth, Input, Lastelement),
     mc(Depth, Lastelement, Returnpen).
 
+
+ penalty(1, a, 4).
+ penalty(1, b, 4).
+ penalty(1, c, 5).
+ penalty(2, d, 22).
+ penalty(3, e, 21).
+ penalty(1, f, 7).
+ person(name,age).
 	
+youngest(penalty(Task, Machine, Penalty)) :-
+	penalty(Task, Machine, Penalty),
+  	\+  (penalty(Task2, Machine2, Penalty2),Penalty2 < Penalty).
+
+findPen(task, mach) :-
+	penalty(task, mach, X).
