de64d36293e5a6e23a6f56d28c06181a4b6cc38c
Jason Wiker <jason.wiker@gmail.com>
2018-04-14 20:58:29 -0600

    Merge jésus code

--- tester.pl
+++ tester.pl
@@ -22,10 +22,25 @@ machine(6).
 machine(7).
 machine(8).
 
-toonear(a,a).
+toonear(z,x).
+%toonear(a,b).
+%toonear(g,f).
+%toonear(b,c).
+%toonear(d,e).
+
 forcedpartial(a,0).
+%forcedpartial(a,2).
+%forcedpartial(b,2).
+%forcedpartial(c,3).
+%forcedpartial(d,4).
+%forcedpartial(e,5).
+
 forbidden(a,0).
-toonearp(a,a,0).
+%forbidden(a,2).
+%forbidden(b,2).
+%forbidden(c,2).
+%forbidden(d,2).
+%forbidden(e,2).
 
 findlowest:-
 	forall(testnode(X,Y),anus(X,Y)).
@@ -59,31 +74,14 @@ correctassign([]).
 correctassign([T|H]) :-
 	task(T),
 	correctassign(H).
-	
-%toonear(a,b).
-%toonear(g,f).
-%toonear(b,c).
-%toonear(d,e).
-
-%forcedpartial(a,1).
-%forcedpartial(b,2).
-%forcedpartial(c,3).
-%forcedpartial(d,4).
-%forcedpartial(e,5).
-
-%forbidden(a,2).
-%forbidden(b,2).
-%forbidden(c,2).
-%forbidden(d,2).
-%forbidden(e,2).
 
 hardcons(List):-
-	not(tntcheck(List)),
+	tntcheck(List),
 	fpcheck(List),
 	fmcheck(List).
 
 tntcheck(List):-
-	forall(toonear(X,Y),closecheck(List,X,Y)).
+	forall(toonear(X,Y),not(closecheck(List,X,Y))).
 
 length_1(0,[]).
 length_1(L+1, [H|T]) :- length_1(L,T).
@@ -91,14 +89,14 @@ length_1(L+1, [H|T]) :- length_1(L,T).
 closecheck(List,X,Y):-
 	X==Y;
 	proper_length(List,8),
-	Imputlist = [H|T],
+	List = [H|T],
+	Y==H,
 	reverse(List,Rlist),
 	Rlist = [HR|TR],
-	X==HR,
-	Y==H;
+	X==HR;
 	length_1(L,List),
 	M is L,
-	(M==1->true;
+	(M==1->false;
 	nth1(M,List,W),	
 	N is L-1,
 	nth1(N,List,V),
@@ -121,25 +119,41 @@ forbiddencheck(List,X,Y):-
 	proper_length(List,Y),
 	not(last(List,X)).
 
-nodegrow(8,Y,Z) :-
-	asserta(node(8,Y,Z)).
-nodegrow(X,Y,Z) :-
+/*nodegrow(8,Y) :-
+	asserta(node(8,Y)).
+nodegrow(X,Y) :-
 	write("nodegrow"),
 	write(Y),
 	X2 is X+1,
 	subtract([a,b,c,d,e,f,g,h],Y,Remaining),
-	branchalot(X2,Y,Z,Remaining).
+	branchalot(X2,Y,Remaining).
 	
-badnode([a,b]).
-	
-branchalot(X,Y,Z,[]).
-branchalot(X,Y,Z,[H|T]) :-
+branchalot(X,Y,[]).
+branchalot(X,Y,[H|T]) :-
 	write("branchalot"),
 	write(Y),
 	append(Y,[H],Total),
-	length(Y,Len),
+	nodegrow(X,Total),
+	branchalot(X,Y,T).*/
+
+nodegrow(8,Y) :-
+	asserta(node(8,Y)).
+nodegrow(X,Y) :-
+	write("nodegrow"),
+	write(Y),
+	X2 is X+1,
+	subtract([a,b,c,d,e,f,g,h],Y,Remaining),
+	branchalot(X2,Y,Remaining).
+	
+%badnode([a,b]).
+	
+branchalot(X,Y,[]).
+branchalot(X,Y,[H|T]) :-
+	write("branchalot"),
+	write(Y),
+	append(Y,[H],Total),
 	machinepen(Y,Z,Depth),
-	(not(badnode(Total))->nodegrow(X,Total,Z),branchalot(X,Y,Z,T);
+	(hardcons(Total)->nodegrow(X,Total),branchalot(X,Y,T);
 	branchalot(X,Y,Z,T)).
 
 masterrule(Depth, Assignment ) :-
